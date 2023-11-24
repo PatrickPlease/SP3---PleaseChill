a73117b112382ff1e32d258650c6bccab23f7b13
Andreas Frølund Mogensen <cph-am526@cphbusiness.dk>
2023-11-24 10:26:21 +0100

    andreas arbejde

--- src/Category.java
+++ src/Category.java
@@ -6,14 +6,11 @@ public class Category {
 
     public void searchByName(String name, List<TvShow> tvShows, List<Movies> movies) {
         boolean foundResults = false;
-        List<Integer> tvShowIndexes = new ArrayList<>();
-        List<Integer> movieIndexes = new ArrayList<>();
 
         ui.displayMessage("Search results in TV Shows:");
         for (int i = 0; i < tvShows.size(); i++) {
             if (tvShows.get(i).getTitle().toLowerCase().contains(name.toLowerCase().trim())) {
                 ui.displayMessage((i + 1) + ". " + tvShows.get(i).getTitle() + " - Release year: " + tvShows.get(i).getStartYear() + "-" + tvShows.get(i).getEndYear());
-                tvShowIndexes.add(i);
                 foundResults = true;
             }
         }
@@ -22,42 +19,51 @@ public class Category {
         for (int i = 0; i < movies.size(); i++) {
             if (movies.get(i).getTitle().toLowerCase().contains(name.toLowerCase().trim())) {
                 ui.displayMessage((i + 1) + ". " + movies.get(i).getTitle() + " - Release year: " + movies.get(i).getReleaseYear());
-                movieIndexes.add(i);
+
                 foundResults = true;
             }
         }
 
         if (!foundResults) {
             ui.displayMessage("No results found for '" + name + "'.");
-        } else {
-            ui.displayMessage("Indexes for TV Shows: " + tvShowIndexes);
-            ui.displayMessage("Indexes for Movies: " + movieIndexes);
         }
+
     }
 
 
     public void searchByGenre(String genre, List<TvShow> tvShows, List<Movies> movies) {
+        boolean foundResults = false;
+
         ui.displayMessage("Search results in TV Shows by Genre:");
         for (TvShow tvShow : tvShows) {
             if (tvShow.getGenre().toLowerCase().contains(genre.toLowerCase())) {
                 ui.displayMessage(tvShow.getTitle() + " - Release year: " + tvShow.getStartYear() + "-" + tvShow.getEndYear());
+                System.out.println();
+                foundResults = true;
             }
         }
 
         ui.displayMessage("Search results in Movies by Genre:");
+
         for (Movies movie : movies) {
             if (movie.getGenre().toLowerCase().contains(genre.toLowerCase())) {
                 ui.displayMessage(movie.getTitle() + " - Release year: " + movie.getReleaseYear());
                 System.out.println();
+                foundResults = true;
             }
+        }
+        if (!foundResults) {
+            ui.displayMessage("No results found for '" + genre + "'.");
         }
     }
 
     public void searchByYear(int year, List<TvShow> tvShows, List<Movies> movies) {
+        boolean foundResults = false;
         ui.displayMessage("Search results in TV Shows by Year:");
         for (TvShow tvShow : tvShows) {
             if (tvShow.getStartYear() <= year && tvShow.getEndYear() >= year) {
                 ui.displayMessage(tvShow.getTitle() + " - Release year: " + tvShow.getStartYear() + "-" + tvShow.getEndYear());
+                foundResults = true;
             }
         }
 
@@ -65,7 +71,11 @@ public class Category {
         for (Movies movie : movies) {
             if (movie.getReleaseYear() == year) {
                 ui.displayMessage(movie.getTitle() + " - Release year: " + movie.getReleaseYear());
+                foundResults = true;
             }
+        }
+        if (!foundResults) {
+            ui.displayMessage("No results found for '" + year + "'.");
         }
     }
 }
